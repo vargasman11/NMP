72cc4153778584e0850ea40ae1723b26ef85e9c2
Jerry Vargas <vargasman11@gmail.com>
2022-02-16 20:15:15 -0600

    add

--- web/js/main.js
+++ web/js/main.js
@@ -20,42 +20,7 @@ __webpack_require__.r(__webpack_exports__);
   \*****************************/
 /***/ (() => {
 
-// $('#mobile-nav__menu-btn').click(function(){
-//     // $('#nav > ul').toggleClass('show grow');
-//     $('#nav > ul').animate({
-//         height:'300px'
-//     }, 500);
-// });
-
-
-$('#mobile-nav__menu-btn').click(function () {
-    // $('#nav > ul').toggle(
-    $('.mobile-nav__primary-menu').toggle(
-        function () {
-            $('#nav > ul').animate({
-                height: 'auto'
-            }, 300)
-        },
-
-        function () {
-            // $('#nav > ul').animate({
-            $('.mobile-nav__primary-menu').animate({
-                height: '200px'
-            }, 300)
-        }
-    )});
-
-
-
-$(window).resize(function(){
-
-    if ($(window).width() >= 978) {
-        $('.mobile-nav__primary-menu').css('display','none');
-        //    alert('window is small');
-
-    }
-
-});
+const nav = document.getElementById("nav");
 
 /***/ })
 
